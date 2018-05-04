898a5b570a808112345c7482ae27148c7991b9e2
c.bbardysh <c.bbardysh@gogoair.com>
2018-05-04 21:41:50 +0300

    add additional files

---
+++ main_app/settings.py
@@ -0,0 +1,8 @@
+
+
+NASA_EONET = {
+    'categories': 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories',
+    'events': 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events',
+    'sources': 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/sources',
+    'layers': 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/layers',
+}
